8a78d3ac8d25966fa264918ba9998db12eb7b917
Lakshay Bansal <lakshay012003@gmail.com>
2024-04-26 22:31:41 +0530

    first commit

---
+++ 5.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+    crossorigin="anonymous">
+
+
+
+
+</head>
+<body>
+    
+ 
+
+    <?php
+function sp(){
+    echo "<br/><br/>";
+}
+
+$arr=array_fill(0,10,1);
+print_r($aarr);
+
+$arr=
+
+function solrlogic($a,$b){
+    if $a==$b
+    return 0;
+    else($a>$b)?-1:1;
+
+}
+
+$arr2=array("r"=>"red","b"=>"blue","g"=>"green");
+uksort($arr2,"sortlogic");
+sp();
+print_r($arr2);
+
+
+
+
+
+
+
+$arr=array(26,12,3,65,78,44);
+print_r($arr);
+
+$arr2=array(12,32,13,14,54,23,43);
+asort($arr2);
+sp();
+print_r($arr2);
+
+$students=array(
+    "king"=>array("c"=>12,"cpp"=>22,"python"=>34),
+    "james"=>array("c"=>25,"cpp"=>29,"python"=>30)
+    "neena"=>array("c"=>22,"cpp"=>21,"python"=>12));
+
+
+    foreach($students as $k=>$name){
+        echo"<h4>$k</h4>: "
+        foreach($name as $ns=>$marks){
+            echo"$name-$marks&nbsp;&nbsp;"
+        print("</br>");
+
+    }
+
+
+
+
+    // if(isset($_GET["sbt"])){
+    //     $c1=$_GET["chk"];
+    //     print_r($c1);
+
+    // }
+    // $arr=array(10,20,30,40,50);
+    // echo "$arr[0]  $arr[1]";
+    // print_r($arr);
+
+
+    // for($i=0;$i<count($arr);$i++){
+    //     echo $arr[i]."</br>";
+    // }
+
+
+//     for($i;$i<6;$i++){
+//         print($arr[$i]."</br>");
+//     }
+
+//     foreach($arr as $i){
+//         print($i."</br>");
+//     }
+// $students=array(
+//     "king"=>array("c"=>12,"cpp"=>22,"python"=>34),
+// "james"=>array("c"=>25,"cpp"=>29,"python"=>30)
+// "neena"=>array("c"=>22,"cpp"=>21,"python"=>12));
+
+// print_r($students["king"][0])
+
+
+
+
+
+
+
+
+function demo($num1,$num2){
+    if($num1%2==0 and $num2%2==0){
+        print($num1+$num2);
+    }
+    else{
+        print("only eleven nums are allowed");
+    }
+}
+
+declare(strict_types=1); //must be the 1st statement of the php funtion in order to make it work .. V.V.Imppppppportanntttttt
+if(isset($_GET['sbt']))
+{
+    $val1=$_GET['txt'];
+    $val2=$_GET['txt']
+}
+
+
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ 7.2.php
@@ -0,0 +1,147 @@
+<?php
+$conn=mysqli_connect("localhost","root","","hr","3308")or die("some error");
+
+if(isset($_GET["search"]))
+{
+    $eid=$_GET['txtsearch'] ;
+    $selqry="select * from employee where uid=$eid";
+    $rs=mysqli_query($conn,$selqry);
+    $row=mysqli_fetch_array($rs);
+    //echo("$row[0] $row[1] $row[2] $row[3] $row[4] $row[5] $row[6] $row[7]");
+    //print("<script>alert($eid);</script>");
+    
+}
+?>
+
+
+<?php if(isset($row)){echo $row[0];}  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+        crossorigin="anonymous">     
+    <title>Starter template</title>
+</head>
+<body class="bg-light">
+    <div class="container w-50">
+        <h1>Update Profile</h1>
+        <div class="bg-dark text-white" style="border-radius:10px">
+            <form class="p-3">
+            <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text"><input class='btn btn-outline-primary btn-block p-0' type='submit' value="Search"  name='search' />
+                </span>
+            </div>
+            <input type="text" name="txtsearch" id="" class="form-control"  placeholder="" aria-describedby="helpId">
+
+            </div>
+
+            <div class="form-group">
+                <label for="">User Id</label>
+                <input type="text" name="txtuid" id="" class="form-control"  placeholder="" aria-describedby="helpId"  value="<?php if(isset($row)){echo $row[0];}  ?>" >
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="form-group">
+                <label for="">Name</label>
+                <input type="text" name="txtname" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?php if(isset($row)){echo $row[1];}  ?>">
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="form-group">
+                <label for="">Password</label>
+                <input type="password" name="txtpassword" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?php if(isset($row)){echo $row[2];}  ?>">
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="form-group">
+                <label for="">Full Name</label>
+                <input type="text" name="txtfullname" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?php if(isset($row)){echo $row[3];}  ?>">
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="email" name="txtemail" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?php if(isset($row)){echo $row[4];}  ?>">
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="form-group">
+                <label for="">Mobile</label>
+                <input type="number" name="txtmobile" id="" class="form-control" placeholder="" aria-describedby="helpId" value="<?php if(isset($row)){echo $row[5];}  ?>">
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="form-group">
+                <label for="">Address</label>
+                <textarea  name="txtaddress" id="" class="form-control" placeholder="" aria-describedby="helpId"><?php if(isset($row)){echo $row[6];}  ?></textarea>
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="form-group">
+                <label for="">Upload Image</label>
+                <input type="file" name="fil" class="form-control-file"/>
+                <small id="helpId" class="text-muted">Help text</small>
+            </div>
+            <div class="row">
+                <div class="col"><input class='btn btn-outline-primary btn-block my-4' type='submit'  name='insert' value='Insert' /></div>
+                <div class="col"><input class='btn btn-outline-primary btn-block my-4' type='submit'  name='update' value='Update' /></div>
+                <div class="col"><input class='btn btn-outline-primary btn-block my-4' type='submit'  name='delete' value='Delete' /></div>
+                <div class="col"><input class='btn btn-outline-primary btn-block my-4' type='submit'  name='show' value='Show' /></div>
+            
+            <div>    
+        </form>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+        crossorigin="anonymous"></script>
+</script>
+    </body>
+
+</html>
+
+<?php
+if(isset($_GET["insert"]))
+{
+    $uid=$_GET['txtuid'];
+    $name=$_GET['txtname'];
+    $password=$_GET['txtpassword'];
+    $fullname=$_GET['txtfullname'];
+    $email=$_GET['txtemail'];
+    $mobile=$_GET['txtmobile'];
+    $address=$_GET['txtaddress'];
+    $image='./image/image(3).jpeg';
+   // echo "$uid $name $password $fullname $email $mobile $address $image";
+    $insqry="insert into employee values($uid,'$name','$password','$fullname','$email','$mobile','$address','$image')";
+    mysqli_query($conn,$insqry)or die(mysqli_error());
+}
+
+if(isset($_GET["update"]))
+{
+    $uid=$_GET['txtuid'];
+    $name=$_GET['txtname'];
+    $password=$_GET['txtpassword'];
+    $fullname=$_GET['txtfullname'];
+    $email=$_GET['txtemail'];
+    $mobile=$_GET['txtmobile'];
+    $address=$_GET['txtaddress'];
+    $image='./image/image(3).jpeg';
+   // echo "$uid $name $password $fullname $email $mobile $address $image";
+    $updqry="update employee set uname='$name',password='$password',fullname='$fullname',email='$email',mobile='$mobile',address='$address',image='$image' where uid=$uid";
+    mysqli_query($conn,$updqry)or die(mysqli_e,rror());
+}
+if(isset($_GET["delete"]))
+{
+    $uid=$_GET['txtuid'];
+    $delqry="delete from  employee where uid=$uid";
+    mysqli_query($conn,$delqry)or die(mysqli_error());
+}    
+if(isset($_GET["show"]))
+{
+    print("<script>alert('show button is clicked');</script>");
+}
+
+
+?>
